4752a0a411d04aa0a4908b4e41db5dc945e91005
Nader Tak <120279049+nadertak7@users.noreply.github.com>
2024-03-29 17:53:41 +0000

    Channge muteables to normal attributes

--- utils/controller.py
+++ utils/controller.py
@@ -5,8 +5,8 @@ import pyautogui
 
 class Controller:
     def __init__(self) -> None:
-        self.enable_script_muteable = [False]
-        self.close_script_muteable = [False]
+        self.enable_script_state = False
+        self.close_script_state = False
 
     def introduce(self):
         print("Press F8 to Toggle Auto-Accept and F9 to Close")
@@ -17,23 +17,20 @@ class Controller:
         else:
             print("Script Disabled")
 
-    def invert_muteable(self, list_var) -> None:
-        list_var[0] = not list_var[0]
-
     def toggle_script(self) -> None:
-        self.invert_muteable(self.enable_script_muteable)
-        self.script_state_prompt(self.enable_script_muteable[0])
+        self.enable_script_state = not self.enable_script_state
+        self.script_state_prompt(self.enable_script_state)
 
     def close_script(self) -> None:
-        self.invert_muteable(self.close_script_muteable)
+        self.close_script_state = not self.close_script_state
 
     def add_hotkeys(self) -> None:
         keyboard.add_hotkey('f8', self.toggle_script)
         keyboard.add_hotkey('f9', self.close_script)
 
     def accept_game_loop(self) -> None:
-        while not self.close_script_muteable[0]:
-            while self.enable_script_muteable[0] and not self.close_script_muteable[0]:
+        while not self.close_script_state:
+            if self.enable_script_state and not self.close_script_state:
                 center_res = (
                     ctypes.windll.user32.GetSystemMetrics(0)/2,
                     ctypes.windll.user32.GetSystemMetrics(1)/2
